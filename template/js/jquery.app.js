86b92c2871380787792f334b4ddf547197f70cbd
nttruongqn <nttruongqn@gmail.com>
2021-01-05 22:04:24 +0700

    fix

--- template/js/jquery.app.js
+++ template/js/jquery.app.js
@@ -57,53 +57,6 @@
 }(window.jQuery),
 
 
-//portlets
-function($) {
-    "use strict";
-
-    /**
-    Portlet Widget
-    */
-    var Portlet = function() {
-        this.$body = $("body"),
-        this.$portletIdentifier = ".portlet",
-        this.$portletCloser = '.portlet a[data-toggle="remove"]',
-        this.$portletRefresher = '.portlet a[data-toggle="reload"]'
-    };
-
-    //on init
-    Portlet.prototype.init = function() {
-        // Panel closest
-        var $this = this;
-        $(document).on("click",this.$portletCloser, function (ev) {
-            ev.preventDefault();
-            var $portlet = $(this).closest($this.$portletIdentifier);
-                var $portlet_parent = $portlet.parent();
-            $portlet.remove();
-            if ($portlet_parent.children().length == 0) {
-                $portlet_parent.remove();
-            }
-        });
-
-        // Panel Reload
-        $(document).on("click",this.$portletRefresher, function (ev) {
-            ev.preventDefault();
-            var $portlet = $(this).closest($this.$portletIdentifier);
-            // This is just a simulation, nothing is going to be reloaded
-            $portlet.append('<div class="panel-disabled"><div class="loader-1"></div></div>');
-            var $pd = $portlet.find('.panel-disabled');
-            setTimeout(function () {
-                $pd.fadeOut('fast', function () {
-                    $pd.remove();
-                });
-            }, 500 + 300 * (Math.random() * 5));
-        });
-    },
-    //
-    $.Portlet = new Portlet, $.Portlet.Constructor = Portlet
-    
-}(window.jQuery),
- 
 
 //main app module
 function($) {
