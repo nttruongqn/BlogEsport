d3d09d4b01b88328a9ee3963b81fcb701e366997
nttruongqn <nttruongqn@gmail.com>
2021-01-07 22:49:33 +0700

    js

--- js/switcher.js
+++ js/switcher.js
@@ -8,11 +8,4 @@ function setActiveStyleSheet(title) {
   }
 }
 
-function getActiveStyleSheet() {
-  var i, a;
-  for(i=0; (a = document.getElementsByTagName("link")[i]); i++) {
-    if(a.getAttribute("rel").indexOf("style") != -1 && a.getAttribute("title") && !a.disabled) return a.getAttribute("title");
-  }
-  return null;
-}
 
